7a6ff1a417156449d51f8493edc1eba6a6a09a52
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-06-28 12:20:51 +0300

    africas talking node

---
+++ one_sms/index.js
@@ -0,0 +1,30 @@
+const credentials = {
+    apiKey : 'Api_Key',
+    username: 'my_first_smsapp',
+}
+
+
+try {
+    
+const Africastalking = require('africastalking')(credentials)
+const sms = Africastalking.SMS;
+
+function sendMessage() {
+    const options = {
+        to: ['+254759614089'],
+        message: "I a developer",
+        // from: "XXYYZZ"
+    }
+    sms.send(options)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+sendMessage()
+} catch (error) {
+    
+}
